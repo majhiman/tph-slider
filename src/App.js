a785ea51449e880d29dcd11dc44335ab3d65e032
majhiman <63633050+majhiman@users.noreply.github.com>
2020-08-20 12:24:20 +0530

    the first try
    Error:-
    ./src/components/slider.js
      Line 46:25:   'classNames' is not defined  no-undef
      Line 51:28:   'classNames' is not defined  no-undef
      Line 105:18:  'ReactDOM' is not defined    no-undef

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+//import {slides} from './Component/slider.js';
+import CitiesSlider from './components/slider.js';
+
+function App() {
+    return (
+        <div>
+            < CitiesSlider/>
+            <div id="app"></div>
+        </div>
+    );
+  }
+  
+  export default App;
